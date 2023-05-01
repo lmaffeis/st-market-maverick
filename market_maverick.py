43f533525b153394d1d037c4bea018087d57f95f
lmaffeis <52122735+lmaffeis@users.noreply.github.com>
2023-05-01 18:41:01 +0200

    Add files via upload

--- market_maverick.py
+++ market_maverick.py
@@ -5,9 +5,34 @@ import yfinance as yf
 import plotly.express as px
 import ta
 from datetime import datetime, timedelta
-from scraper import tickers
+
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# Make an HTTP request to the Wikipedia page
+response = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find the table that contains the component stocks tickers
+table = soup.find("table", {"id": "constituents"})
+
+# Find all the rows in the table except the header row
+rows = table.find_all("tr")[1:]
+
+# Extract the tickers from the rows and store them in a list
+tickers = []
+for row in rows:
+    ticker = row.find_all("td")[0].text.strip()
+    tickers.append(ticker)
+
+# Sort the tickers alphabetically and store it in a new variable
+tickers.sort()
 
 # Set up the page layout
 st.set_page_config(
